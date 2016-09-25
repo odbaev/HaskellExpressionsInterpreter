919b9075987b99eed94903270fd7f4a85406935e
Oleg Baev <odbaev@yandex.ru>
2016-09-25 11:32:34 +0300

    update templates

--- HaskellExpressionsInterpreter/Resources/TemplateInitial.hs
+++ HaskellExpressionsInterpreter/Resources/TemplateInitial.hs
@@ -1,19 +1,7 @@
 import Control.Applicative
-import Control.Monad.Identity
+import Data.Functor.Identity
 import Data.Traversable
 import Debug.SimpleReflect
-
-
-parens :: String -> String
-parens s = "(" ++ s ++ ")"
-
-instance Show m => Show (Const m a) where
-  show (Const x) = "Const " ++ addParens (show x)
-    where addParens s = if ' ' `elem` s then parens s else s
-
-instance Show a => Show (Identity a) where
-  show (Identity x) = "Identity " ++ addParens (show x)
-    where addParens s@(x:_) = if x /= '(' && ' ' `elem` s then parens s else s
 
 
 main :: IO ()
