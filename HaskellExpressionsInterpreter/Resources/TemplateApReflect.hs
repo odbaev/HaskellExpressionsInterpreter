cd43017cb55020c4ef9c0a9a834d8d3f2aa5925d
Oleg Baev <odbaev@yandex.ru>
2016-09-25 21:54:31 +0300

    Revert "update templates"
    This reverts commit 919b9075987b99eed94903270fd7f4a85406935e.

--- HaskellExpressionsInterpreter/Resources/TemplateApReflect.hs
+++ HaskellExpressionsInterpreter/Resources/TemplateApReflect.hs
@@ -1,11 +1,19 @@
 {-# LANGUAGE TypeOperators #-}
 
 import Control.Applicative
-import Data.Functor.Identity
+import Control.Monad.Identity
 
 import Debug.SimpleReflect
 import Debug.Reflect
 
+
+instance Show m => Show (Const m a) where
+  show (Const x) = "Const " ++ addParens (show x)
+    where addParens s = if ' ' `elem` s then parens s else s
+
+instance Show a => Show (Identity a) where
+  show (Identity x) = "Identity " ++ addParens (show x)
+    where addParens s@(x:_) = if x /= '(' && ' ' `elem` s then parens s else s
 
 -- (+)
 (.+) :: (Num a, Show a) => a ~> a ~> a
@@ -79,13 +87,13 @@ import Debug.Reflect
 (.!!) :: (Show a) => [a] ~> Int ~> a
 (.!!) = makeBinOp "!!" (!!)
 
-sequenceA' :: (Show a, Show (f a), Show (f [a]), Show (f ([a] ~> [a])), Applicative f) => [f a] -> Ap (f [a])
-sequenceA' [] = pure'' []
-sequenceA' (x:xs) = Val ap' :$ ((.:) -$- x) :$ sequenceA' xs
+sequenceA :: (Show a, Show (f a), Show (f [a]), Show (f ([a] ~> [a])), Applicative f) => [f a] -> Ap (f [a])
+sequenceA [] = pure'' []
+sequenceA (x:xs) = Val ap' :$ ((.:) -$- x) :$ sequenceA xs
 
-traverse' :: (Show a, Show b, Show (f b), Show (f [b]), Show (f ([b] ~> [b])), Applicative f) => (a -> f b) -> [a] -> Ap (f [b])
-traverse' _ [] = pure'' []
-traverse' f (x:xs) = Val ap' :$ (Val fmap' :$ Val (.:) :$ fx) :$ traverse' f xs
+traverse :: (Show a, Show b, Show (f b), Show (f [b]), Show (f ([b] ~> [b])), Applicative f) => (a -> f b) -> [a] -> Ap (f [b])
+traverse _ [] = pure'' []
+traverse f (x:xs) = Val ap' :$ (Val fmap' :$ Val (.:) :$ fx) :$ traverse f xs
   where fx = Val (Fn "f" f) :$ Val x
 
 
